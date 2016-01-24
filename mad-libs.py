969bb56466a1131d9412f6ce59b9c32804e42215
[Jonny Choi] <jonnychoi@seoulchristian.org>
2016-01-24 19:34:53 +0900

    Fix mad-libs.py

--- mad-libs.py
+++ mad-libs.py
@@ -1,15 +1,17 @@
-#Type a subject
+# Type a subject
 print "What is your name?",
 subj = raw_input("> ")
-#Type a verb
+# Type a verb
 print "What are you doing now?",
 verb = raw_input("> ")
-#Type a location
+# Type a location
 print "Where are you now?",
 location = raw_input("> ")
-#Type an adjective
-print "How do you look now? (With Capitalization)"
+# Type an adjective
+print "How do you look now?"
 adj = raw_input("> ")
-#Insert everything in order
-print "%r %r is %r in %r." % (
-    adj, subj, verb, location)
+# Insert everything in order
+print "One day, there was a monster named %r." % (subj)
+print "%r was %r and scary." % (subj, adj)
+print "When %r %r appears, the girls ran away because %r %r was %r in %r." % (
+    adj, subj, adj, subj, verb, location)
